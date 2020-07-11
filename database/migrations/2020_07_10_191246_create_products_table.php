28c1d855a4afc5d7c598c7998c0d772f71a45d73
Brero, Massimiliano <massimiliano@adess.de>
2020-07-11 11:53:29 +0200

    first commit

--- database/migrations/2020_07_10_191246_create_products_table.php
+++ database/migrations/2020_07_10_191246_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->text('detail');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
